8cbe1ab6f166835ff8dd7f256203b99500b321aa
chauchuty <chauchuty@alunos.utfpr.edu.br>
2022-09-24 17:57:34 -0300

    updated: login

--- api/src/handle.message.ts
+++ api/src/handle.message.ts
@@ -1,35 +1,50 @@
 import ProtocolRequest from "./model/protocol.request";
 import ProtocolResponse from "./model/protocol.response";
+import prisma from './prisma/connection'
 
 class HandleMessage {
+    private response!: ProtocolResponse
+    private request!: ProtocolRequest
 
     handleProtocolRequest(data: Buffer){
         return new Promise<string>(async (resolve, reject) => {
             try {
-                const protocolRequest = JSON.parse(data.toString()) as ProtocolRequest
-                let response
-                switch (protocolRequest.operacao) {
+                this.request = JSON.parse(data.toString()) as ProtocolRequest
+                
+                switch (this.request.operacao) {
                     case 'login':
-                        response = await this.handleLogin(protocolRequest.params)
+                        this.response = await this.handleLogin(this.request.params)
                         break;
                     case 'register':
-                        response = await this.handleRegister(protocolRequest.params)
+                        // this.response = await this.handleRegister(protocolRequest.params)
                         break;
                     case 'logout':
-                        response = await this.handleLogout(protocolRequest.params)
+                        // this.response = await this.handleLogout(protocolRequest.params)
                         break;
                     default:
                         break;
                 }
-                resolve(new ProtocolResponse(200, 'OK', response).toJson())
+                resolve(this.response.toJson())
             } catch (error) {
                 // reject(error)
             }
         })
     }
 
-    async handleLogin(params: any){
-        // ...await 
+    async handleLogin(params: any): Promise<ProtocolResponse>{
+        const { ra, senha } = params
+        const usuario = await prisma.usuario.findFirst({
+            where: {
+                ra: ra,
+                senha: senha
+            }
+        })
+
+        if(usuario){
+            return new ProtocolResponse(200, 'Usuário logado com sucesso!', usuario)
+        } else {
+            return new ProtocolResponse(404, 'Usuário ou senha inválido!', null)
+        }
     }
 
     async handleRegister(params: any){
